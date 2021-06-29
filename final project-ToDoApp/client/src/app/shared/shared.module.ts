a49c510a906642e41b6a83977a63a63d3ee7a403
shmuelkohanian <82038355+shmuelkohanian@users.noreply.github.com>
2021-06-29 20:43:40 +0300

    Latest version

--- final project-ToDoApp/client/src/app/shared/shared.module.ts
+++ final project-ToDoApp/client/src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ErrorsPresenterComponent } from './components/errors-presenter/errors-presenter.component';
-import { TodoItemPresenterComponent } from '../components/todo-item-presenter/todo-item-presenter.component';
+import { TodoItemPresenterComponent } from './components/todo-item-presenter/todo-item-presenter.component';
 
 
 
